f7b8cb41934f25a0505f9cc126f6e689344c62e5
hydrol0x <34951139+hydrol0x@users.noreply.github.com>
2021-09-24 16:55:21 -0400

    Update swapping.py

--- swapping.py
+++ swapping.py
@@ -5,6 +5,6 @@ for i in range(x):
   last = str1[-1] 
   first = str1[0]
 
-  str1 = str1.replace(last, first)
-  output = str1.replace(first, last)
+  str1.replace(last, first)
+  str1.replace(first, last)
   print(output)
