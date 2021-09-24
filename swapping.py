449efd477eb5b4b0182c3a6bd1c1b044acf70232
hydrol0x <34951139+hydrol0x@users.noreply.github.com>
2021-09-24 16:02:53 -0400

    Update swapping.py

--- swapping.py
+++ swapping.py
@@ -1,11 +1,8 @@
 str1 = str(input())
-str1 = str1.split(" ")
+
 last = str1[-1] 
 first = str1[0]
 
-str1.pop(0)
-str1.pop(-1)
-
-output = str1.append(first)
-output = output.insert(0, last)
+str1 = str1.replace(last, first)
+output = str1.replace(first, last)
 print(output)
